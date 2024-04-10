9566e9327706a52e7a5b2721a33e79071643c0cf
DambiermontStephane <stephane.dambiermont@student.hepl.be>
2024-04-10 13:34:37 +0200

    Add some things

--- src/ts/asteroids/drawables/Ship.ts
+++ src/ts/asteroids/drawables/Ship.ts
@@ -2,33 +2,42 @@ import {Triangle} from "../../framework/shapes/Triangle";
 import {IAnimatable} from "../../framework/types/IAnimatable";
 import {settings} from "../settings";
 import {KeyController} from "../KeyController";
+import {Vectors} from "../../framework/Vectors";
 
 
 export class Ship extends Triangle implements IAnimatable {
     private readonly canvas: HTMLCanvasElement;
     private keyController: KeyController;
+    private speed: Vectors;
+    private direction: number;
 
     constructor(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, keyController: KeyController) {
-        super(ctx, canvas.width / 2, canvas.height / 2, settings.ship.width, settings.ship.height, settings.ship.color, false);
+        super(ctx, {
+            x: canvas.width / 2,
+            y: canvas.height / 2
+        }, settings.ship.width, settings.ship.height, settings.ship.color, 0, false);
         this.canvas = canvas;
+        this.speed = new Vectors({
+            x: 0,
+            y: 0,
+        });
         this.keyController = keyController;
         this.draw();
     }
 
     update(): void {
-        this.y -= 2;
         //this.checkEdges();
     }
 
     private checkEdges() {
-        if (this.x - this.width / 2 >= this.canvas.width) {
-            this.x = -this.width / 2;
-        } else if (this.x + this.width / 2 <= 0) {
-            this.x = this.canvas.width + this.width / 2;
-        } else if (this.y + this.height / 2 <= 0) {
-            this.y = this.canvas.height + this.height / 2;
-        } else if (this.y - this.height / 2 >= this.canvas.height) {
-            this.y = -this.height / 2
+        if (this.position.x - this.width / 2 >= this.canvas.width) {
+            this.position.x = -this.width / 2;
+        } else if (this.position.x + this.width / 2 <= 0) {
+            this.position.x = this.canvas.width + this.width / 2;
+        } else if (this.position.y + this.height / 2 <= 0) {
+            this.position.y = this.canvas.height + this.height / 2;
+        } else if (this.position.y - this.height / 2 >= this.canvas.height) {
+            this.position.y = -this.height / 2
         }
     }
 }
--- src/ts/framework/shapes/Circle.ts
+++ src/ts/framework/shapes/Circle.ts
@@ -15,4 +15,16 @@ export class Circle extends Shape {
         this.ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
         this.fillOrStroke();
     }
+
+    public clear() {
+        this.ctx.save();
+        this.ctx.translate(this.position.x, this.position.y);
+        this.ctx.rotate(this.degree);
+        if (this.isFilled) {
+            this.ctx.clearRect(-this.radius, -this.radius, this.radius, this.radius);
+        } else {
+            //ToDo:implement
+        }
+        this.ctx.restore();
+    }
 }
